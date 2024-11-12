686a5a55fc9f0a24ef44bc9e0a51633d54a2b7ff
IlgizBikbaev <131395377+IlgizBikbaev@users.noreply.github.com>
2024-11-12 09:42:52 +0300

    Add files via upload

--- module_3_0.py
+++ module_3_0.py
@@ -1,15 +1,19 @@
 def calculate_structure_sum(data_structure):
     s = 0
     s = float(s)
-    if isinstance(data_structure, (dict, list, set, tuple)):
+    if isinstance(data_structure, (list, set, tuple)):
         for element in data_structure:
            s+= calculate_structure_sum(element)
     elif isinstance(data_structure, (int, float)):
         s += data_structure
     elif isinstance(data_structure, str):
         s += len(data_structure)
+    # elif isinstance(data_structure, dict): # мой старый вариант, но он не учитывает тип данных в ключах и значениях, cумма та же
+    #     s += sum(map(len,list(data_structure.keys())))+ sum(list(data_structure.values()))
     elif isinstance(data_structure, dict):
-         s += sum(list(data_structure.values()))
+        for key, value in data_structure.items():
+            s += calculate_structure_sum(key)
+            s += calculate_structure_sum(value)
     return s
 
 
